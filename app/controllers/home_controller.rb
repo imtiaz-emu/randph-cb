66dc7889cf9e7bb27077ab321f9d575acf312c55
imtiaz-emu <imtiaz.emu.it@gmail.com>
2018-03-28 04:32:14 +0600

    bug fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,10 +5,14 @@ class HomeController < ApplicationController
   end
 
   def rand_ph
+
     while(1)
       @new_phrase = Phrase.all.sample.name
       if session[:rand_ph].nil?
         return (session[:rand_ph] = @new_phrase)
+      end
+      if session[:rand_ph].length == 10
+        return session[:rand_ph]
       end
       unless session[:rand_ph].include?(@new_phrase)
         return (session[:rand_ph] = session[:rand_ph] + @new_phrase)
